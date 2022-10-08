0ec95c605847001d8648513979b2c0a533ec1b10
Mr-KayZ <kazi.muttakin@ucalgary.ca>
2022-10-08 02:49:35 -0600

    Some stuff

--- Lecture notes (HTML format)/1_1_hello_world.py
+++
@@ -1,6 +0,0 @@
-# This is the most basic python code you can think of
-# Python files have the extension .py.
-# Comments are written using #
-# Very easy to get into. For example, here is a very simple code:
-
-print("Hello World!")
